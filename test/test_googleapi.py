d6af839a4e51c5339dac8c4cf883267531f1483d
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-28 21:46:50 +0200

    Test suite for googleapi

---
+++ test/test_googleapi.py
@@ -0,0 +1,10 @@
+"""Unit test suite for the Google API part."""
+import unittest2 as unittest
+
+from maemogcalsync import googleapi
+
+class TestGoogleApi(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
