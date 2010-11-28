9283ed958f9dce1d591440bf7c4edec0ed809be2
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-28 21:50:08 +0200

    Add info expected by setup.py

--- maemogcalsync/__init__.py
+++ maemogcalsync/__init__.py
@@ -1,1 +1,7 @@
 """Python modules implementing two-way sync between Maemo and Google Calendar."""
+
+__author__='Ilkka Laukkanen'
+__version__='0.0.1'
+__author_email__='ilkka.s.laukkanen@gmail.com'
+__url__='http://github.com/ilkka/maemogcalsync'
+
