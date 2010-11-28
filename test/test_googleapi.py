13008f91eeb7adfce61549abbfcb29adc2ea8d9f
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-28 22:15:39 +0200

    Simple test for getting events

--- test/test_googleapi.py
+++ test/test_googleapi.py
@@ -1,10 +1,20 @@
 """Unit test suite for the Google API part."""
 import unittest2 as unittest
+from datetime import datetime, timedelta
 
 from maemogcalsync import googleapi
 
 class TestGoogleApi(unittest.TestCase):
-    pass
+    """Test suite class"""
+    def test_get_new_events(self):
+        """Test fetching new events.
+
+        Fetches last 20 days of events. Probably has trouble if no events
+        exist within the last 20 days."""
+        lastsync = datetime.utcnow() - timedelta(days=20)
+        events = googleapi.get_events_since(lastsync)
+        self.assertGreater(len(events), 0)
+
 
 if __name__ == "__main__":
     unittest.main()
