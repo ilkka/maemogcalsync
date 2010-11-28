1d6165891f4ca549d2446392ee7454fa440fd2b1
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-28 21:52:54 +0200

    Spaces around operators

--- maemogcalsync/__init__.py
+++ maemogcalsync/__init__.py
@@ -1,7 +1,6 @@
-"""Python modules implementing two-way sync between Maemo and Google Calendar."""
+"""Python modules for two-way sync between Maemo and Google Calendar."""
 
-__author__='Ilkka Laukkanen'
-__version__='0.0.1'
-__author_email__='ilkka.s.laukkanen@gmail.com'
-__url__='http://github.com/ilkka/maemogcalsync'
-
+__author__ = 'Ilkka Laukkanen'
+__version__ = '0.0.1'
+__author_email__ = 'ilkka.s.laukkanen@gmail.com'
+__url__ = 'http://github.com/ilkka/maemogcalsync'
