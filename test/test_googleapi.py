02665a6ff7957db4e0dc01ee76877206ceb6c4f0
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-29 10:28:19 +0200

    Add test for getting user calendars

--- test/test_googleapi.py
+++ test/test_googleapi.py
@@ -6,13 +6,19 @@ from maemogcalsync import googleapi
 
 class TestGoogleApi(unittest.TestCase):
     """Test suite class"""
+    def test_get_user_calendars(self):
+        """Test fetching users's calendars."""
+        calendars = googleapi.get_user_calendars()
+        self.assertGreater(len(calendars), 0)
+
     def test_get_new_events(self):
         """Test fetching new events.
 
         Fetches last 20 days of events. Probably has trouble if no events
         exist within the last 20 days."""
         lastsync = datetime.utcnow() - timedelta(days=20)
-        events = googleapi.get_events_since(lastsync)
+        calendars = googleapi.get_user_calendars()
+        events = googleapi.get_events_since(calendars[0], lastsync)
         self.assertGreater(len(events), 0)
 
 
