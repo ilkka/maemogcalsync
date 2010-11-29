d82aee22f59574c6f9d8fb46e56cab6c2f05d3e0
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-29 17:44:47 +0200

    Make tests pass

--- maemogcalsync/googleapi.py
+++ maemogcalsync/googleapi.py
@@ -1,9 +1,10 @@
 """Google API module of maemogcalsync.
 
 This module takes care of interfacing with the Google APIs."""
+from maemogcalsync.event import Event
 
 def get_events_since(calendar, last_sync):
-    return ['event1', 'event2']
+    return [Event(), Event()]
 
 def get_user_calendars():
     return ['calendar1', 'calendar2']
