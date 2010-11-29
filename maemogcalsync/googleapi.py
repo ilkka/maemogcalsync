f6410b8b31b4291a3b19af1b6b11f6df905b3e68
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-11-29 10:30:39 +0200

    Make events, calendars tests green

--- maemogcalsync/googleapi.py
+++ maemogcalsync/googleapi.py
@@ -1,3 +1,9 @@
 """Google API module of maemogcalsync.
 
 This module takes care of interfacing with the Google APIs."""
+
+def get_events_since(calendar, last_sync):
+    return ['event1', 'event2']
+
+def get_user_calendars():
+    return ['calendar1', 'calendar2']
